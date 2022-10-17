7952582efcf6637594df0b96dd372117c3ea8612
Mj <855434@gmail.com>
2022-10-17 23:07:01 +0300

    added Seminar2

--- src/main/java/ru/gb/jseminar/Task0.java
+++ src/main/java/ru/gb/jseminar/Task0.java
@@ -1,5 +1,8 @@
 package ru.gb.jseminar;
 
+import java.util.Scanner;
+import java.util.logging.Logger;
+
 public class Task0 {
 
     // Посчитайте сколько драгоценных камней в куче обычных камней
@@ -7,12 +10,60 @@ public class Task0 {
     // jewels = “aB”, stones = “aaaAbbbB”
     // Результат в консоль ”a3B1”
     public static void main(String[] args) {
-
+        Task0 task = new Task0();
+        Logger log = Logger.getLogger(Task0.class.getName());
+//        log.info(task.findJewelsInStones(jewel, stone));
     }
 
     public String findJewelsInStones(String jewels, String stones) {
 
-        return "";
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input jewels: ");
+        String jewel = sc.nextLine();
+        System.out.print("Input stones: ");
+        String stone = sc.nextLine();
+
+        int count = 0;
+        String result = "";
+
+        for (char j: jewel.toCharArray()) {
+            for (char s: stone.toCharArray()) {
+                if (jewel == stone) {
+                    count +=1;
+                }
+            }
+            result += jewel + Integer.toString(count);
+            count = 0;
+        }
+        return result;
     }
 
 }
+
+
+//package ru.gb.jseminar;
+//import java.util.logging.Logger;
+//public class Task0 {
+// Посчитайте сколько драгоценных камней в куче обычных камней
+// Пример:
+// jewels = “aB”, stones = “aaaAbbbB”
+// Результат в консоль ”a3B1”
+// public static void main(String[] args) {
+//    Task0 task = new Task0();
+//    String jewels = "aB";
+//    String stones = "aaaAbbbB";
+//    Logger log = Logger.getLogger(Task0.class.getName());
+//    log.info(task.findJewelsInStones(jewels, stones)); }
+//
+//    public String findJewelsInStones(String jewels, String stones)
+//    { int count = 0; String result = "";
+//        for (char jewel: jewels.toCharArray()) {
+//            for (char stone: stones.toCharArray()) {
+//                if (jewel == stone) { count += 1; }
+//            }
+//            result += jewel + Integer.toString(count);
+//            count = 0;
+//        }
+//        return result;
+//    }
+//}
--- src/main/java/ru/gb/jseminar/Task3.java
+++ src/main/java/ru/gb/jseminar/Task3.java
@@ -1,5 +1,9 @@
 package ru.gb.jseminar;
 
+import java.io.*;
+import java.net.URL;
+import java.util.logging.Logger;
+
 public class Task3 {
 
     // Напишите программу, которая
@@ -9,24 +13,60 @@ public class Task3 {
     // 4) читает сохраненный файл и выводит содержимое в логгер
     // 5) удаляет сохраненный файл
     public static void main(String[] args) {
+        Task3 t3 = new Task3();
         String fileUrl = "https://raw.githubusercontent.com/aksodar/JSeminar_2/master/src/main/resources/example.txt";
+        String fileContent = t3.download(fileUrl);
+        String name = "Ivan";
+        fileContent = t3.change(name, fileContent);
+        t3.saveOnLocal("example.txt", fileContent);
+        t3.read("example.txt");
 
     }
 
-    public String download(String url, String localFilename) {
+    public String download(String urlText) {
+        try (
+                BufferedInputStream bf = new BufferedInputStream(new URL(urlText).openStream());
+        ) {
+            String result = new String(bf.readAllBytes());
+        return result;
 
-        return "";
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
-
     public String change(String name, String fileContent) {
-
-        return "";
+        return String.format(name, fileContent);
     }
 
     public void saveOnLocal(String fileName, String fileContent) {
+        try {
+            FileWriter fw = new FileWriter(fileName);
+            BufferedWriter bfw = new BufferedWriter(fw);
+            bfw.write(fileContent);
+            bfw.flush();
+            fw.close();
 
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
+    public void read(String localFileName){
+        try {
+            FileReader fr = new FileReader(localFileName);
+            BufferedReader bfr = new BufferedReader(fr);
+            Logger log = Logger.getLogger(Task3.class.getName());
+            while (bfr.readLine() != null) {
+                log.info(bfr.readLine());
+
+            }
+            fr.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
     public boolean removeFromLocale(String fileName) {
 
         return false;
