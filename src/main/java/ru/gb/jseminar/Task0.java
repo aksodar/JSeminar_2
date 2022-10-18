1643795ea796a8b37f0131846e73d60d8cb60241
Marina Chernykh <marina.u.chernykh@gmail.com>
2022-10-19 00:59:38 +0300

    hw done

--- src/main/java/ru/gb/jseminar/Task0.java
+++ src/main/java/ru/gb/jseminar/Task0.java
@@ -1,18 +1,35 @@
 package ru.gb.jseminar;
 
+import java.util.logging.Logger;
+
+
+
 public class Task0 {
 
     // Посчитайте сколько драгоценных камней в куче обычных камней
     // Пример:
     // jewels = “aB”, stones = “aaaAbbbB”
     // Результат в консоль ”a3B1”
+    
     public static void main(String[] args) {
-
+        Task0 task0 = new Task0();
+        String jewels = "aB", stones = "aaaAbbbB";
+        String result = task0.findJewelsInStones(jewels, stones);
+        Logger log = Logger.getLogger(Task0.class.getName());
+        log.info(result);
     }
 
     public String findJewelsInStones(String jewels, String stones) {
-
-        return "";
+        String result = " ";
+        for (char jewel : jewels.toCharArray()){
+            int counter = 0;
+            for (char stone : stones.toCharArray()){
+                if (jewel == stone) counter +=1;
+                }
+            result += String.valueOf(jewel);
+            result += counter;
+        }
+        return result;
     }
 
 }
--- src/main/java/ru/gb/jseminar/Task3.java
+++ src/main/java/ru/gb/jseminar/Task3.java
@@ -1,5 +1,10 @@
 package ru.gb.jseminar;
 
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Logger;
+
 public class Task3 {
 
     // Напишите программу, которая
@@ -9,25 +14,54 @@ public class Task3 {
     // 4) читает сохраненный файл и выводит содержимое в логгер
     // 5) удаляет сохраненный файл
     public static void main(String[] args) {
+        Task3 t3 = new Task3();
         String fileUrl = "https://raw.githubusercontent.com/aksodar/JSeminar_2/master/src/main/resources/example.txt";
-
+        String fileContent = t3.download(fileUrl);
+        String name = "Marina";
+        fileContent = t3.change(name, fileContent);
+        t3.saveOnLocal("example.txt", fileContent);
+        t3.read("example.txt");
     }
 
-    public String download(String url, String localFilename) {
-
-        return "";
+    public String download(String url) {
+        try (
+            BufferedInputStream bf = new BufferedInputStream(new URL(url).openStream());
+        ){
+            String result = new String(bf.readAllBytes());
+            return result;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public String change(String name, String fileContent) {
-
-        return "";
+        return String.format(fileContent, name);
     }
 
     public void read(String localFilename){
-
+        try {
+            FileReader rf = new FileReader(localFilename);
+            BufferedReader bfr = new BufferedReader(rf);
+            Logger log = Logger.getLogger(Task3.class.getName());
+            while(bfr.ready()) {
+                log.info(bfr.readLine());
+            }
+            rf.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void saveOnLocal(String fileName, String fileContent) {
+        try {
+            FileWriter fw = new FileWriter(fileName);
+            BufferedWriter bfw = new BufferedWriter(fw);
+            bfw.write(fileContent);
+            bfw.flush();
+            fw.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
