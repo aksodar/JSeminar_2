c516fef44c0133fc2e78a630eb5a10b590b68d6d
dmitry kuznetsov <dmitriik06@bk.ru>
2022-10-22 20:43:57 +0300

    Half of homework task is completed

--- src/main/java/ru/gb/jseminar/Homework.java
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -1,5 +1,7 @@
 package ru.gb.jseminar;
 
+import java.io.IOException;
+
 public class Homework {
 
     // Дана строка sql-запроса "select * from students".
@@ -8,12 +10,39 @@ public class Homework {
     // 1) Данные для фильтрации подаются в метод двумя массивами paramName и paramValue.
     // 2) Данные для фильтрации подаются в метод строкой в формате json.
     public static void main(String[] args) {
-
+        String sqlRequest = "select * from students";
+        Homework HW2 = new Homework();
+        String[] str2 = {"1", "2"};
+        String[] str1 = {"2", "1"};
+//        String[] str2 = {"0"};
+//        String[] str2 = {};
+        System.out.println(HW2.updateQueryByArrays(sqlRequest, str1, str2));
     }
 
-    public String updateQueryByArrays(String q, String[] paramName, String[] paramValue){
-
-        return "";
+    public String updateQueryByArrays(String q, String[] paramName, String[] paramValue) {
+        if (paramName.length == 0 || paramValue.length == 0) {
+            return q;
+        } else {
+            try {
+                if (paramName.length != paramValue.length){
+                    throw new ArrayIndexOutOfBoundsException();
+                }
+                StringBuilder sb = new StringBuilder(q);
+                sb.append(" WHERE ");
+                for (int i = 0; i < paramName.length; i++) {
+                    sb.append(paramName[i]);
+                    sb.append("=");
+                    sb.append(paramValue[i]);
+                    if (i != paramName.length - 1) {
+                        sb.append(" AND ");
+                    }
+                }
+                return sb.toString();
+            } catch (ArrayIndexOutOfBoundsException exception) {
+                System.out.println(exception);
+                return null;
+            }
+        }
     }
 
     public String updateQueryByJson(String q, String[] paramName, String[] paramValue){
