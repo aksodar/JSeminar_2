c6e472388cadac400c880165c9af80427f4ad773
MaxLeontev <lmaks21@mail.ru>
2022-10-23 20:58:58 +0300

    add homework

--- src/main/java/ru/gb/jseminar/Homework.java
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -8,12 +8,29 @@ public class Homework {
     // 1) Данные для фильтрации подаются в метод двумя массивами paramName и paramValue.
     // 2) Данные для фильтрации подаются в метод строкой в формате json.
     public static void main(String[] args) {
-
+        updateQueryByArrays();
     }
 
-    public String updateQueryByArrays(String q, String[] paramName, String[] paramValue){
-
-        return "";
+    public StringBuilder updateQueryByArrays(String q, String[] paramName, String[] paramValue){
+        StringBuilder result= new StringBuilder();
+        result.append(q);
+        if (paramName==null || paramValue==null) {
+            return result;
+        }
+        result.append("WHERE");
+        if (paramName.length!=paramValue.length){
+            throw new ArrayIndexOutOfBoundsException();
+        }else{
+            for (int i = 0; i < paramName.length; i++) {
+                result.append(paramName[i]);
+                result.append("=");
+                result.append(paramValue[i]);
+                if (i!= paramName.length-1) {
+                    result.append("and");
+                }
+            }
+        }
+        return result;
     }
 
     public String updateQueryByJson(String q, String[] paramName, String[] paramValue){
