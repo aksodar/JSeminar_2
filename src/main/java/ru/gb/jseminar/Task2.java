80348a092a678110dbb16988449287a352fa91d1
Inna Osipova <rutheva_inna@mail.ru>
2022-10-23 15:53:58 +0300

    Task2

--- src/main/java/ru/gb/jseminar/Task2.java
+++ src/main/java/ru/gb/jseminar/Task2.java
@@ -1,5 +1,12 @@
 package ru.gb.jseminar;
 
+
+import java.util.InputMismatchException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.logging.Logger;
+
 public class Task2 {
 
     //Напишите программу, которая принимает с консоли число в формате byte и записывает его в файл ”result.txt”.
@@ -7,12 +14,25 @@ public class Task2 {
     //
     //После написания, попробуйте подать на вход числа 100 и 200 и проследите разницу в результате
     public static void main(String[] args) {
-
+        Task2 task2 = new Task2();
+        Logger log = Logger.getLogger(Task2.class.getName());
+        log.severe(task2.writeFile());
     }
 
-    public String findJewelsInStones(String jewels, String stones){
+    public String writeFile () {
+        Byte number = 0;
+        try (FileWriter fw = new FileWriter("result.txt", false)) { 
+            Scanner iScanner = new Scanner(System.in);
+            System.out.println("Введите число : ");
+            number = iScanner.nextByte();
+            fw.write(number.toString());
+            fw.flush(); 
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        } catch (InputMismatchException valueOf){
+            System.out.println("Введите другое значение");
+        }
+        return number.toString();
+        } 
         
-        return "";
-    }
-
 }
