12c919a651f3f6eeb29c3626988bddaf478a6936
Nadezhda <nbykova79@gmail.com>
2022-10-23 12:55:59 +0300

    первая задача

--- src/main/java/ru/gb/jseminar/Task0.java
+++ src/main/java/ru/gb/jseminar/Task0.java
@@ -1,18 +1,35 @@
 package ru.gb.jseminar;
 
+import java.util.logging.Logger;
+
 public class Task0 {
 
     // Посчитайте сколько драгоценных камней в куче обычных камней
     // Пример:
     // jewels = “aB”, stones = “aaaAbbbB”
+    
     // Результат в консоль ”a3B1”
     public static void main(String[] args) {
-
+        Task0 task = new Task0();
+        String jewels = "Ab";
+        String stones = "aaaaAAbbbB";
+        Logger log = Logger.getLogger(Task0.class.getName());
+        log.info(task.findJewelsInStones(jewels, stones));       
     }
 
     public String findJewelsInStones(String jewels, String stones) {
-
-        return "";
+        int count = 0;
+        String result = "";
+        for (char jewel: jewels.toCharArray()) {
+            for (char stone: stones.toCharArray()) {
+                if (stone == jewel){
+                    count +=1;
+                }
+            }
+            result +=jewel+Integer.toString(count);
+            count=0;
+        }
+        return result;
     }
 
 }
