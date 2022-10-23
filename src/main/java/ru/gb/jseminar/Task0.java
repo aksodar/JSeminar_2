2926e9d08cd205e85842dabd529f2761fa31170f
Vladimir_Artamonov <tomodachiva@gmail.com>
2022-10-23 21:41:00 +0300

    Task0 of 2 Seminar was done

--- src/main/java/ru/gb/jseminar/Task0.java
+++ src/main/java/ru/gb/jseminar/Task0.java
@@ -1,5 +1,7 @@
 package ru.gb.jseminar;
 
+import java.util.logging.Logger;
+
 public class Task0 {
 
     // Посчитайте сколько драгоценных камней в куче обычных камней
@@ -7,12 +9,35 @@ public class Task0 {
     // jewels = “aB”, stones = “aaaAbbbB”
     // Результат в консоль ”a3B1”
     public static void main(String[] args) {
+        Task0 task = new Task0();
+        Logger log = Logger.getLogger(Task0.class.getName());
+
+        // log.warning("WARNING");
+        // log.severe("ERROR");
+        // log.info("INFO");
+        String jewels = "aB";
+        String stones = "aaaAbbbbB";
+        log.info(task.findJewelsInStones(jewels, stones));
+        // System.out.println(task.findJewelsInStones(jewels, stones));
 
     }
 
     public String findJewelsInStones(String jewels, String stones) {
+        String result = "";
+        int jewelsCount = 0;
+        for (char element : jewels.toCharArray()) {
+            for (char st : stones.toCharArray()) {
+                if (st == element) {
+                    jewelsCount++;
+                }
+                // result = element + Integer.toString(jewelsCount);
+            }
+            result += element + Integer.toString(jewelsCount);
+            jewelsCount = 0;
 
-        return "";
+        }
+
+        return result;
     }
 
 }
