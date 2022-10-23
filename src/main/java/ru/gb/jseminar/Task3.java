ac3cc4b26c5548d1cc7afb1c296b15c04f1d1299
Inna Osipova <rutheva_inna@mail.ru>
2022-10-23 19:59:45 +0300

    Task3

--- src/main/java/ru/gb/jseminar/Task3.java
+++ src/main/java/ru/gb/jseminar/Task3.java
@@ -1,5 +1,14 @@
 package ru.gb.jseminar;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+import java.util.logging.Logger;
+
 public class Task3 {
 
     // Напишите программу, которая
@@ -9,25 +18,59 @@ public class Task3 {
     // 4) читает сохраненный файл и выводит содержимое в логгер
     // 5) удаляет сохраненный файл
     public static void main(String[] args) {
+        Task3 task3 = new Task3();
         String fileUrl = "https://raw.githubusercontent.com/aksodar/JSeminar_2/master/src/main/resources/example.txt";
+        String fileContent = task3.download(fileUrl);
+        String name = "Inna";
+        fileContent = task3.change(name, fileContent);
+        task3.saveOnLocal("exemple.txt", fileContent);
+        task3.read("example.txt");
 
     }
 
-    public String download(String url, String localFilename) {
+    public String download(String urlText) {
+        try (
+            BufferedInputStream bf = new BufferedInputStream(new URL(urlText).openStream());
+        ){
+            String result = new String(bf.readAllBytes());
+            return result;
+        } catch(IOException e){
+            throw new RuntimeException(e);
+        }
 
-        return "";
+       
     }
 
     public String change(String name, String fileContent) {
 
-        return "";
+        return  String.format(fileContent, name);
     }
 
     public void read(String localFilename){
+        try{
+            FileReader rf = new FileReader(localFilename);
+            BufferedReader bfr = new BufferedReader(rf);
+            Logger log = Logger.getLogger(Task3.class.getName());
+            while(bfr.readLine() != null){
+                log.info(bfr.readLine());
+            }
+            rf.close();
+        } catch (IOException e){
+            throw new RuntimeException(e);
+        }
 
     }
 
     public void saveOnLocal(String fileName, String fileContent) {
+        try{
+            FileWriter fw = new FileWriter(fileName);
+            BufferedWriter bfw = new BufferedWriter(fw);
+            bfw.write(fileContent);
+            bfw.flush();
+            fw.close();
+        } catch (IOException e){
+            throw new RuntimeException(e);
+        }
 
     }
 
