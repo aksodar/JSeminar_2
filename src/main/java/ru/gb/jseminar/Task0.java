56ad1a60bbe3d0bf291163247452ec25842c3c5a
Naganishe <mr.nordakov@mail.ru>
2022-10-23 20:08:09 +0300

    add Homework

--- src/main/java/ru/gb/jseminar/Task0.java
+++ src/main/java/ru/gb/jseminar/Task0.java
@@ -1,18 +1,69 @@
 package ru.gb.jseminar;
 
+import java.util.logging.Logger;
+
 public class Task0 {
 
     // Посчитайте сколько драгоценных камней в куче обычных камней
     // Пример:
     // jewels = “aB”, stones = “aaaAbbbB”
     // Результат в консоль ”a3B1”
-    public static void main(String[] args) {
+//     public static void main(String[] args) {
+//         // Logger log = Logger.getLogger(Task0.class.getName);
+//         // log.warning("Hello");
+//         // log.severe("ERROR");
+//         // log.info("INFO");
+//         Task0 task = new Task0();
+//         String jewels = "aB"
+//         String stones = "aaaAbbbB";
+//         Logger log = Logger.getLogger(Task0.class.getName());
+//         log.info(task.findJewelsInStones(jewels, stones));
+//     }
+
+//     public String findJewelsInStones(String jewels, String stones) {
+//         int count = 0;
+//         String result = "";
+
+//         for (char jewel: jewels.toCharArray()) {
+//             for(char stone: stones.toCharArray()) {
+//                 if (jewel == stone) {
+//                     count += 1;
+//                 }
+//             }
+//             result += jewel + Integer.toCharArray(count);
+//         }
+//         return result;
+        
+//     }
+// }
+
+public static void main(String[] args) {
+    Task0 task = new Task0();
+
+    Logger log = Logger.getLogger(Task0.class.getName());
+    int count = 0;
+    String r = "";
+    log.info(task.findJewelsInStones("aB", "aaaAbbbB"));
+
+}
+
+public String findJewelsInStones(String jewels, String stones) {
+    String result = "";
+    int count = 0;
+
+    for (char jaw: jewels.toCharArray()){
+        result += jaw;
+        for (char stone: stones.toCharArray()){
+            if (jaw == stone){
+
+                count ++;
+            }
+        }
+        result += count;
+        count = 0;
 
     }
-
-    public String findJewelsInStones(String jewels, String stones) {
-
-        return "";
-    }
+    return result;
+}
 
 }
