a5f7535c51eae62c4e6c64d11a5cb979d29cdcea
Oleg Osipov <vopisso@gmail.com>
2022-10-24 18:52:37 +0300

    Homework is done

--- src/main/java/ru/gb/jseminar/Homework.java
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -8,16 +8,62 @@ public class Homework {
     // 1) Данные для фильтрации подаются в метод двумя массивами paramName и paramValue.
     // 2) Данные для фильтрации подаются в метод строкой в формате json.
     public static void main(String[] args) {
+        Homework hw = new Homework();
 
+        String request = "SELECT * FROM students";
+        String[] name = {"Ivan", "Petr", "Nikita"};
+        String[] value = {"Ivanov", "Petrov", "Osipov"};
+        String jsonString = "{\"Ivan\": \"Ivanov\", \"Petr\": \"Petrov\", \"Nikita\": \"Osipov\"}";
+
+        System.out.println("1) Данные для фильтрации подаются в метод двумя массивами paramName и paramValue: ");
+        System.out.println(hw.updateQueryByArrays(request, name, value));
+        System.out.println("\n2) Данные для фильтрации подаются в метод строкой в формате json: ");
+        System.out.println(hw.updateQueryByJson(request, jsonString));
     }
 
     public String updateQueryByArrays(String q, String[] paramName, String[] paramValue){
-
-        return "";
+        if (paramName.length == 0 || paramValue.length == 0) {
+            return q;
+        } else {
+            try {
+                if (paramName.length != paramValue.length){
+                    throw new ArrayIndexOutOfBoundsException();
+                }
+                StringBuilder sb = new StringBuilder(q);
+                sb.append(" WHERE ");
+                for (int i = 0; i < paramName.length; i++) {
+                    sb.append(paramName[i]);
+                    sb.append("=");
+                    sb.append(paramValue[i]);
+                    if (i != paramName.length - 1) {
+                        sb.append(" AND ");
+                    }
+                }
+                return sb.toString();
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println(e);
+                return null;
+            }
+        }
     }
 
-    public String updateQueryByJson(String q, String[] paramName, String[] paramValue){
-
-        return "";
+    public String updateQueryByJson(String q, String js){
+        if (js.length() == 0){
+            return q;
+        } else {
+            StringBuilder sb = new StringBuilder(q);
+            sb.append(" WHERE ");
+            js = js.substring(1, js.length() - 1);
+            String[] jsonStr = js.split(", ");
+            for (int i = 0; i < jsonStr.length; i++) {
+                jsonStr[i] = jsonStr[i].substring(1, jsonStr[i].length() - 1);
+                jsonStr[i] = jsonStr[i].replace("\": \"", "=");
+                sb.append(jsonStr[i]);
+                if (i < jsonStr.length - 1){
+                    sb.append(" AND ");
+                }
+            }
+            return sb.toString();
+        }
     }
 }
--- src/main/java/ru/gb/jseminar/Task3.java
+++ src/main/java/ru/gb/jseminar/Task3.java
@@ -1,5 +1,11 @@
 package ru.gb.jseminar;
 
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.logging.Logger;
+
 public class Task3 {
 
     // Напишите программу, которая
@@ -9,21 +15,60 @@ public class Task3 {
     // 4) читает сохраненный файл и выводит содержимое в логгер
     // 5) удаляет сохраненный файл
     public static void main(String[] args) {
+        Task3 t3 = new Task3();
         String fileUrl = "https://raw.githubusercontent.com/aksodar/JSeminar_2/master/src/main/resources/example.txt";
+        String fileContent = t3.download(fileUrl);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введи свое имя: ");
+        String name = sc.nextLine();
+        fileContent = t3.change(name, fileContent);
+        t3.saveOnLocal("example.txt", fileContent);
+        t3.read("example.txt");
 
     }
 
-    public String download(String url, String localFilename) {
+    public String download(String urlText) {
+        try (
+                BufferedInputStream bf = new BufferedInputStream(new URL(urlText).openStream());
+        ){
+            String result = new String(bf.readAllBytes());
 
-        return "";
+            return result;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public String change(String name, String fileContent) {
+        return String.format(fileContent, name);
+    }
 
-        return "";
+    public void read(String localFilename){
+        try {
+            FileReader rf = new FileReader(localFilename);
+            BufferedReader bfr = new BufferedReader(rf);
+            Logger log = Logger.getLogger(Task3.class.getName());
+//            while(bfr.readLine() != null) {
+//                log.info(bfr.readLine());
+//            }
+            log.info(bfr.readLine());
+            rf.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     public void saveOnLocal(String fileName, String fileContent) {
+        try {
+            FileWriter fw = new FileWriter(fileName);
+            BufferedWriter bfw = new BufferedWriter(fw);
+            bfw.write(fileContent);
+            bfw.flush();
+            fw.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
