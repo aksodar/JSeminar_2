08e486d73243c8d5ad7403381b62b8703461ad12
LeaClover <plesen_14@mail.ru>
2022-10-24 15:44:23 +0300

    edit project task1

--- src/main/java/ru/gb/jseminar/Task1.java
+++ src/main/java/ru/gb/jseminar/Task1.java
@@ -1,5 +1,7 @@
 package ru.gb.jseminar;
 
+import java.util.logging.Logger;
+
 public class Task1 {
 
     // Вам дается строка S и целочисленный массив индексов int index[s.length].
@@ -8,12 +10,19 @@ public class Task1 {
     //
     // Пример: s = “cba”, index = [3,2,1] result “abc”
     public static void main(String[] args) {
-
+        Task1 task = new Task1();
+        String s = "cba";
+        int[] arrIndex = new int[] {3, 2, 1};
+        Logger log = Logger.getLogger(Task1.class.getName());
+        log.info(task.shuffle(s, arrIndex));
     }
 
-    public String shuffle(final String s, final int[] index){
-
-        return "";
+    public String shuffle(String s, int[] index){
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < index.length; i++)  {
+            sb.append(s.charAt(index[i] - 1));
+        }
+        return sb.toString();
     }
 
 }
