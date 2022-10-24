a23269f8f3d5cc41580ed29b594eaee28b8ad160
LeaClover <plesen_14@mail.ru>
2022-10-24 15:44:01 +0300

    edit project homework

--- src/main/java/ru/gb/jseminar/Homework.java
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -1,4 +1,5 @@
 package ru.gb.jseminar;
+import java.util.logging.Logger;
 
 public class Homework {
 
@@ -10,16 +11,53 @@ public class Homework {
     //      Пример json: {"firstName": "Ivan", "secondName": "Ivanov", ....}
     // Результат работы методов: "select * from students where firstName = 'Ivan' and ...".
     public static void main(String[] args) {
-
+        Homework hw = new Homework();
+        Logger log = Logger.getLogger(Homework.class.getName());
+        String sqlSTR = "SELECT * FROM Students";
+        String[] paramName = new String [] {"firstName", "secondName", "value"};
+        String[] paramValue = new String [] {"Aleksandr", "Aleksandrov", "sales"};
+        String js = "{\"firstName\": \"Alex\", \"secondName\": \"Rakhmanin\", \"value\": \"database\"}";
+        log.info(hw.updateQueryByArrays(sqlSTR, paramName, paramValue));
+        log.info(hw.updateQueryByJson(sqlSTR, js));   
     }
 
     public String updateQueryByArrays(String q, String[] paramName, String[] paramValue){
-
-        return "";
+        StringBuilder sb = new StringBuilder();
+        sb.append(q).append(" WHERE ");
+        for(int i = 0; i < paramName.length; i++) {
+            sb.append(paramName[i] + " = '" + paramValue[i] + "'");
+            if(paramName.length != i + 1) {
+                sb.append(" AND ");
+            }
+            if(paramName.length == i + 1) {
+                sb.append(";");
+            }
+        }
+        return sb.toString();
     }
 
     public String updateQueryByJson(String q, String json){
-
-        return "";
+        StringBuilder sb = new StringBuilder();
+        json = json.substring(1, json.length() - 1).replace(":", "").replace(",", "");
+        String[] arrJS = json.split(" ");
+        sb.append(q).append(" WHERE ");
+        for(int i = 0; i < arrJS.length; i++) {
+            if(i%2 == 0) {
+                arrJS[i] = arrJS[i].substring(1, arrJS[i].length() - 1);
+                sb.append(arrJS[i]);
+                sb.append(" = ");
+            }
+            if(i%2 == 1 && i != arrJS.length - 1) {
+                arrJS[i] = arrJS[i].replace("\"", "'");
+                sb.append(arrJS[i]);
+                sb.append(" AND ");
+            }
+            if(i == arrJS.length - 1) {
+                arrJS[i] = arrJS[i].replace("\"", "'");
+                sb.append(arrJS[i]);
+                sb.append(";");
+            }
+        }
+        return sb.toString();
     }
 }
