ca76fe2ecb8a6b172937c7abcfa2ed5343eef007
Maria <ma.pechenkina@yandex.ru>
2022-10-29 18:55:25 +0300

    homework added

--- src/main/java/ru/gb/jseminar/Homework.java
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -1,5 +1,8 @@
 package ru.gb.jseminar;
 
+import java.util.concurrent.Flow.Subscriber;
+import java.util.concurrent.Flow.Subscription;
+
 public class Homework {
 
     // Дана строка sql-запроса "select * from students".
@@ -10,16 +13,58 @@ public class Homework {
     //      Пример json: {"firstName": "Ivan", "secondName": "Ivanov", ....}
     // Результат работы методов: "select * from students where firstName = 'Ivan' and ...".
     public static void main(String[] args) {
-
+        Homework hw = new Homework();
+        String q = "select * from students";
+        String [] paramName = new String []{"firstName", "secondName"};
+        String [] paramValue = new String []{"\"Ivan\"", "\"Ivanov\""};
+        String json = "{\"firstName\": \"Ivan\", \"secondName\": \"Ivanov\"}";
+        
+        System.out.println(hw.updateQueryByArrays(q, paramName, paramValue)); 
+        System.out.println(hw.updateQueryByJson(q, json)); 
     }
 
     public String updateQueryByArrays(String q, String[] paramName, String[] paramValue){
-
-        return "";
+        StringBuilder sb = new StringBuilder();
+        if (paramName.length == 0 && paramValue.length == 0)
+        {
+            sb.append(q);
+            sb.append(" where");
+        }
+        else if (paramName.length != paramValue.length)
+        {
+            throw new IllegalStateException("Массивы содержат разное количество элементов. Запрос не может быть выполнен.");
+        }
+        else
+        {
+            sb.append(q);
+            sb.append(" where");
+            for (int i = 0; i < paramName.length; i++)
+            {
+                sb.append(" " + paramName[i] + " = " + paramValue[i]);
+                if (i < (paramName.length - 1)) sb.append(" and");
+            }
+        }
+        return sb.toString();
     }
 
     public String updateQueryByJson(String q, String json){
+        json = json.substring(1, (json.length() - 1));
+        String[] subStr;
+        String[] subStr2;
+        String delimeter = ","; 
+        subStr = json.split(delimeter); 
+                
+        String delimeter2 = ":"; 
+        String [] paramName1 = new String [subStr.length];
+        String [] paramValue1 = new String [subStr.length];
 
-        return "";
+        for (int i = 0; i < subStr.length; i++)
+        {
+            subStr2 = subStr[i].split(delimeter2);
+            paramName1[i] = subStr2[0].replaceAll("[\" ]", "");
+            paramValue1[i] = subStr2[1].trim();
+        }
+
+        return updateQueryByArrays(q, paramName1, paramValue1);
     }
 }
--- src/main/java/ru/gb/jseminar/Task1.java
+++ src/main/java/ru/gb/jseminar/Task1.java
@@ -1,5 +1,5 @@
 package ru.gb.jseminar;
-
+import java.util.logging.Logger;
 public class Task1 {
 
     // Вам дается строка S и целочисленный массив индексов int index[s.length].
@@ -8,12 +8,20 @@ public class Task1 {
     //
     // Пример: s = “cba”, index = [3,2,1] result “abc”
     public static void main(String[] args) {
-
+        Task1 task = new Task1();
+        Logger log = Logger.getLogger(Task1.class.getName());    
+        String s = "cba";
+        int [] index = new int [] {3,2,1};
+        log.info(task.shuffle(s, index));
     }
-
+    
     public String shuffle(final String s, final int[] index){
-
-        return "";
-    }
-
+        
+        String result = "";
+        for (int i = 0; i < index.length; i++)
+        {
+            result += s.charAt((index[i]) -1);
+        }        
+        return result;
+    }    
 }
