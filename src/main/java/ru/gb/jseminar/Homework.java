87ca5e2ae3296adc77b5a398418d330876925384
Dmitriy_Pyanzin <lysy_41@mail.ru>
2022-11-03 10:57:20 +0300

    Added_Homework2

--- src/main/java/ru/gb/jseminar/Homework.java
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -1,5 +1,8 @@
 package ru.gb.jseminar;
 
+import java.util.Scanner;
+import java.util.logging.Logger;
+
 public class Homework {
 
     // Дана строка sql-запроса "select * from students".
@@ -10,16 +13,46 @@ public class Homework {
     //      Пример json: {"firstName": "Ivan", "secondName": "Ivanov", ....}
     // Результат работы методов: "select * from students where firstName = 'Ivan' and ...".
     public static void main(String[] args) {
+        Homework hw = new Homework();
+        String[] paramName = {"firsName", "secondName", "age", "city", "id"};
+        String[] paramValue = {"Dmitry", "Pyanzin", "34", "Novomoscowsk", "1"};
+        String json = "{'firsName':'Dmitry', 'secondName':'Pyanzin', 'age':'34', 'city':'Novomoscowsk', 'id':'1'}";
+        String q = "select * from students ";
+        Logger log = Logger.getLogger(Homework.class.getName());
+        log.info("Для фильтрации SQL введите 1, для json введите 2: ");
+        Scanner sc = new Scanner(System.in);
+        int answer = sc.nextInt();
+        sc.close();
 
+        if (answer == 1) {
+            hw.updateQueryByArrays(q, paramName, paramValue);
+        } else if (answer == 2){
+            hw.updateQueryByJson(q, json);
+        } else {
+            log.warning("Нужно вводить 1 или 2");
+        }
     }
 
-    public String updateQueryByArrays(String q, String[] paramName, String[] paramValue){
-
-        return "";
+    public void updateQueryByArrays(String q, String[] paramName, String[] paramValue){
+        StringBuilder sb = new StringBuilder();
+        Logger log = Logger.getLogger(Homework.class.getName());
+        for (int i = 0; i < paramName.length; i++) {
+            log.info(sb.append(q).append(paramName[i]).append(" = ").append(paramValue[i]).toString());
+            sb.delete(0, sb.length() - 1);
+        }
     }
 
-    public String updateQueryByJson(String q, String json){
+    public void updateQueryByJson(String q, String json){
+        StringBuilder sb = new StringBuilder();
+        Logger log = Logger.getLogger(Homework.class.getName());
+        json = json.substring(1, json.length() - 1);
+        String[] jsn = json.split(", ");
+        String[] jsnParse;
 
-        return "";
+        for (String s : jsn) {
+            jsnParse = s.split(":");
+            log.info(sb.append(q).append(jsnParse[0]).append(" = ").append(jsnParse[1]).toString());
+            sb.delete(0, sb.length() - 1);
+        }
     }
 }
--- src/main/java/ru/gb/jseminar/Task2.java
+++ src/main/java/ru/gb/jseminar/Task2.java
@@ -1,5 +1,12 @@
 package ru.gb.jseminar;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.logging.Logger;
+
 public class Task2 {
 
     //Напишите программу, которая принимает с консоли число в формате byte и записывает его в файл ”result.txt”.
@@ -7,12 +14,30 @@ public class Task2 {
     //
     //После написания, попробуйте подать на вход числа 100 и 200 и проследите разницу в результате
     public static void main(String[] args) {
+        try {
+            Scanner sc = new Scanner(System.in);
+            byte num = sc.nextByte();
+            sc.close();
+            Task2 t2 = new Task2();
+            t2.ByteImport(num);
+        }
+        catch (InputMismatchException e) {
+            Logger log = Logger.getLogger(Task2.class.getName());
+            log.warning(("Переполнение типа!"));
+        }
 
     }
 
-    public String findJewelsInStones(String jewels, String stones){
-        
-        return "";
+    public byte ByteImport(byte number){
+        try {
+            FileWriter fileW = new FileWriter("result.txt");
+            BufferedWriter bfw = new BufferedWriter(fileW);
+            bfw.write(number);
+            bfw.flush();
+            fileW.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } return number;
     }
 
 }
