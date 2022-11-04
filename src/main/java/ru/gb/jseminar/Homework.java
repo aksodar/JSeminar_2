30383b0085664c2e9d269f6851bb5673837bfa85
Nadezhda <nbykova79@gmail.com>
2022-11-04 17:12:39 +0300

    запись дом.задания

--- src/main/java/ru/gb/jseminar/Homework.java
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -1,4 +1,6 @@
 package ru.gb.jseminar;
+import java.util.logging.Logger;
+import javax.xml.namespace.QName;
 
 public class Homework {
 
@@ -8,16 +10,47 @@ public class Homework {
     // 1) Данные для фильтрации подаются в метод двумя массивами paramName и paramValue.
     // 2) Данные для фильтрации подаются в метод строкой в формате json.
     public static void main(String[] args) {
+        Homework task = new Homework();
+        String q = "select from students";
+        String[] paramName = {"firstName", "secondName"};
+        String[] paramValue = {"Ivan", "Ivanov"};
+        String json = "{'firstName' : 'Ivan', 'secondName' : 'Ivanov', 'age' : '25'}";
+        Logger log = Logger.getLogger(Homework.class.getName());
+        log.info(task.updateQueryByArrays(q, paramName, paramValue));
+        log.info(task.updateQueryByJson(q, json));
 
     }
 
     public String updateQueryByArrays(String q, String[] paramName, String[] paramValue){
-
-        return "";
+        StringBuilder result = new StringBuilder();
+        result.append(q+" ");
+        result.append("WHERE"+" ");
+        for (int i=0; i<paramName.length; i++){
+            result.append(paramName[i]+" - "+paramValue[i]+" ");
+            if (i<paramName.length-1){
+                result.append("AND ");
+            }
+        }
+        String res= new String(result);
+        return res;
     }
 
-    public String updateQueryByJson(String q, String[] paramName, String[] paramValue){
-
-        return "";
+    public String updateQueryByJson(String q, String json){
+        StringBuilder result = new StringBuilder();
+        result.append(q);
+        result.append(" WHERE"+" ");
+        String [] jsTemp = json.substring(1, json.length()-1).split(", ");
+        String [] jsNew;
+        for (int i = 0; i < jsTemp.length; i++) {
+            jsNew = jsTemp[i].split(" : ");            
+            result.append(jsNew[0].replace("'", ""));
+            result.append(" - ");
+            result.append(jsNew[1].replace("'", ""));
+            if (i < jsTemp.length - 1) {
+                result.append(" AND ");
+            }
+        }
+        String res = result.toString();
+        return res;        
     }
 }
