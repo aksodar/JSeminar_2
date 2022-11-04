b78ef89d7c4a6232d872fa860155c5d7b1fe3a26
Nadezhda <nbykova79@gmail.com>
2022-11-04 10:21:30 +0300

    запись домашки

--- src/main/java/ru/gb/jseminar/Task1.java
+++ src/main/java/ru/gb/jseminar/Task1.java
@@ -1,4 +1,5 @@
 package ru.gb.jseminar;
+import java.util.logging.Logger;
 
 public class Task1 {
 
@@ -8,12 +9,25 @@ public class Task1 {
     //
     // Пример: s = “cba”, index = [3,2,1] result “abc”
     public static void main(String[] args) {
+        Task1 task = new Task1();
+        String S = "cba";
+        int [] index = {3,2,1};
+        Logger log = Logger.getLogger(Task1.class.getName());
+        log.info(task.shuffle(S, index));
 
     }
 
     public String shuffle(final String s, final int[] index){
-
-        return "";
+        char[] result = new char[s.length()+1];
+        char [] str = new char[s.length()];
+        str = s.toCharArray();
+        System.out.println(str);
+        int i=0;
+        for (int unit : index) {
+            result[unit-1] = str[i];
+            i++;
+            }
+        String res = new String(result);
+        return res;
     }
-
 }
