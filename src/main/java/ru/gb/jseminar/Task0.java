0ffa561d5e72aa6380e54d39a583579427012c44
Leo_Amv <leonidamvrosov@yandex.ru>
2022-11-04 23:59:59 +0500

    Task0 completed!

--- src/main/java/ru/gb/jseminar/Task0.java
+++ src/main/java/ru/gb/jseminar/Task0.java
@@ -1,5 +1,7 @@
 package ru.gb.jseminar;
 
+import java.util.logging.Logger;
+
 public class Task0 {
 
     // Посчитайте сколько драгоценных камней в куче обычных камней
@@ -7,12 +9,29 @@ public class Task0 {
     // jewels = “aB”, stones = “aaaAbbbB”
     // Результат в консоль ”a3B1”
     public static void main(String[] args) {
-
+        Task0 task0 = new Task0();
+        Logger log = Logger.getLogger(Task0.class.getName());
+        log.info("HELLO");
+        String jewels = "aB";
+        String stones = "aaaAbbbB";
+        String result = task0.findJewelsInStones(jewels, stones);
+        log.warning(result);
     }
 
     public String findJewelsInStones(String jewels, String stones) {
-
-        return "";
+        char[] jewels_arr = jewels.toCharArray();
+        char[] stones_arr = stones.toCharArray();
+        String res = "";
+        for (char jewel : jewels_arr) {
+            int count = 0;
+            for (char stone : stones_arr) {
+                if (jewel == stone) {
+                    count++;
+                }
+            }
+            res += String.format("%s%d", jewel, count);
+        }
+        return res;
     }
 
 }
