90927d29eb950e04aa4cb744a8cd1f8f344d62ed
Leo_Amv <leonidamvrosov@yandex.ru>
2022-11-07 21:19:41 +0500

    Task3 completed!

--- src/main/java/ru/gb/jseminar/Task1.java
+++ src/main/java/ru/gb/jseminar/Task1.java
@@ -1,19 +1,30 @@
 package ru.gb.jseminar;
 
+import java.util.logging.Logger;
+
 public class Task1 {
 
     // Вам дается строка S и целочисленный массив индексов int index[s.length].
     // Напишите программу, которая перетасует символы в S таким образом,
-    // что символ c i-й позиции переместится на индекс index[i] в результирующей строке.
+    // что символ c i-й позиции переместится на индекс index[i] в результирующей
+    // строке.
     //
     // Пример: s = “cba”, index = [3,2,1] result “abc”
     public static void main(String[] args) {
-
+        Task1 task1 = new Task1();
+        Logger log = Logger.getLogger(Task1.class.getName());
+        String s = "cba";
+        int[] index = { 3, 2, 1 };
+        String result = task1.shuffle(s, index);
+        log.warning(result);
     }
 
-    public String shuffle(final String s, final int[] index){
-
-        return "";
+    public String shuffle(final String s, final int[] index) {
+        String res = "";
+        for (int i = 0; i < s.length(); i++) {
+            res += s.charAt(index[i] - 1);
+        }
+        return res;
     }
 
 }
--- src/main/java/ru/gb/jseminar/Task3.java
+++ src/main/java/ru/gb/jseminar/Task3.java
@@ -1,38 +1,83 @@
 package ru.gb.jseminar;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+import java.util.logging.Logger;
+
 public class Task3 {
 
     // Напишите программу, которая
-    // 1) по указанной строке URL скачивает файл и возвращает строковое содержимое файла
+    // 1) по указанной строке URL скачивает файл и возвращает строковое содержимое
+    // файла
     // 2) изменяет строковое содержимое файла подставляя по маске "%s" ваше имя
     // 3) сохраняет файл локально
     // 4) читает сохраненный файл и выводит содержимое в логгер
     // 5) удаляет сохраненный файл
     public static void main(String[] args) {
+        Task3 task3 = new Task3();
+        Logger log = Logger.getLogger(Task3.class.getName());
         String fileUrl = "https://raw.githubusercontent.com/aksodar/JSeminar_2/master/src/main/resources/example.txt";
-
+        String fileContent = task3.download(fileUrl);
+        String name = "Leo";
+        String changedFileContent = task3.change(name, fileContent);
+        task3.saveOnLocal("example.txt", changedFileContent);
+        task3.read("example.txt");
+        if (task3.removeFromLocale("example.txt")) {
+            log.info("File successfully deleted!");
+        } else {
+            log.info("File not deleted!");
+        }
     }
 
-    public String download(String url, String localFilename) {
-
-        return "";
+    public String download(String urlText) {
+        try (BufferedInputStream bf = new BufferedInputStream(new URL(urlText).openStream());) {
+            String result = new String(bf.readAllBytes());
+            return result;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public String change(String name, String fileContent) {
 
-        return "";
+        return String.format(fileContent, name);
     }
 
-    public void read(String localFilename){
-
+    public void read(String localFilename) {
+        try {
+            FileReader fr = new FileReader(localFilename);
+            BufferedReader bfr = new BufferedReader(fr);
+            Logger log = Logger.getLogger(Task3.class.getName());
+            // while (bfr.readLine() != null) {
+            // log.info(bfr.readLine());
+            // }
+            log.info(bfr.readLine());
+            bfr.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void saveOnLocal(String fileName, String fileContent) {
-
+        try {
+            FileWriter fw = new FileWriter(fileName);
+            BufferedWriter bfw = new BufferedWriter(fw);
+            bfw.write(fileContent);
+            bfw.flush();
+            bfw.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public boolean removeFromLocale(String fileName) {
-
-        return false;
+        File file = new File(fileName);
+        return file.delete();
     }
 }
