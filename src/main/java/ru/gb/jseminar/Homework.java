70223daf649be8388cb25cc2b553f55ff5b12fb6
Framuga93 <alekseydemochkin93@gmail.com>
2022-11-08 19:35:56 +0300

    fix HW and add Task1

--- src/main/java/ru/gb/jseminar/Homework.java
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -1,8 +1,12 @@
 package ru.gb.jseminar;
 
+import netscape.javascript.JSObject;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.logging.Logger;
 
 public class Homework {
@@ -18,34 +22,49 @@ public class Homework {
         Homework hw = new Homework();
         String q = "select * from student where ";
         String[] paramName = {"firstName", "secondName", "phoneNumber", "position"};
-        String[] paramValue = {"Ivan","Ivanov", "987654", "manager"};
-        System.out.printf(hw.updateQueryByArrays(q,paramName,paramValue));
-
-
+        String[] paramValue = {"Ivan", "Ivanov", "987654", "manager"};
+        String json = "{\"firstName\": \"Ivan\", \"secondName\": \"Ivanov\", \"phoneNumber\": \"987654\", \"position\": \"manager\"}";
+        Logger log = Logger.getLogger(Homework.class.getName());
+        log.info(hw.updateQueryByArrays(q,paramName,paramValue));
+        log.info(hw.updateQueryByJson(q,json));
 
     }
 
-    public String updateQueryByArrays(String q, String[] paramName, String[] paramValue){
+    public String updateQueryByArrays(String q, String[] paramName, String[] paramValue) {
         StringBuilder result = new StringBuilder(q);
-//        ArrayList<String> finalString = new ArrayList<>();
-        for (int i = 0; i< paramValue.length; i++ ) {
+        for (int i = 0; i < paramValue.length; i++) {
             result.append(paramName[i]);
-            result.append(" ");
             result.append(" = ");
-            result.append(" ");
-            result.append(paramValue[i] );
-            if (i < paramValue.length - 1) result.append(", ");
+            result.append(paramValue[i]);
+            if (i < paramValue.length - 1) result.append(" and ");
             else result.append(".");
 
 
         }
-        String finalString = result.toString();
-        return finalString;
+        return result.toString();
     }
 
-    public String updateQueryByJson(String q, String json){
+    public String updateQueryByJson(String q, String json) {
+        StringBuilder result = new StringBuilder(q);
+        String jsonSub = json.substring(1, json.length() - 1);
+        String[] jsonSplit = jsonSub.split(", ");
+        for (String words : jsonSplit) {
+            String[] wordsSplit = words.split(": ");
+            for (int i = 0; i < wordsSplit.length; i++) {
+                if (i == 0) {
+                    result.append(wordsSplit[i].toString().substring(1, wordsSplit[i].length()-1));
+                    result.append(" = ");
+                }
+                   if (i == 1) {
+                       result.append(wordsSplit[i].toString().substring(1, wordsSplit[i].length()-1));
+                       result.append(" and ");
+                    }
+                    }
+
+            }
+            result.delete(result.length()-5,result.length()).append(".");
 
 
-        return "";
+        return result.toString();
+        }
     }
-}
--- src/main/java/ru/gb/jseminar/Task1.java
+++ src/main/java/ru/gb/jseminar/Task1.java
@@ -13,20 +13,18 @@ public class Task1 {
         Logger log = Logger.getLogger(Task1.class.getName());
         Task1 task1 = new Task1();
         String s = "cba";
-        int[] index = {3,2,1};
+        int[] index = {2,3,1};
+        log.info(task1.shuffle(s,index));
 
     }
 
     public String shuffle(final String s, final int[] index){
         String result = "";
         char[] ar = s.toCharArray();
-        for (int i = 1 ; i < ar.length; i++){
-            int j = index[i];
-            result += ar;
-
+        for (int i = 0; i < s.length(); i++) {
+            int in = index[i]-1;
+            result += ar[in];
         }
-
-
         return result;
     }
 
